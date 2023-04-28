5773dfdde4edd744247fe5261ec5e0ff34969c9d
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-28 14:53:20 -0700

    deploying

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -55,7 +55,7 @@ const resolvers = {
 
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const updatedUser = wait User.findOneAndUpdate(
+        const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id},
           { $pull: { savedBooks: { bookId } } },
           { new: true }
