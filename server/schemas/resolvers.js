228571fced15b936d11bb5365304e481b4331c15
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-28 13:29:35 -0700

    apollo server implemented

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if(context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-_v-password');
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v-password');
 
         return userData;
       }
@@ -41,7 +41,7 @@ const resolvers = {
 
     saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
-        const updatedUser = await User .findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $push: { savedBooks: bookData }},
           { new: true }
